364e86548189edbc9e31701b169781515105a150
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-27 04:13:55 +0100

    Added basic repository for getting data

---
+++ api/MovieSwipe.API/Data/Models/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieSwipe.API.Data.Models;
+
+public class Tag
+{
+    public Tag(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
---
+++ api/MovieSwipe.API/Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieSwipe.API.Data.Models;
+
+public class User
+{
+    public User(Guid id, string name, DateTime birthdate, string email)
+    {
+        Id = id;
+        Name = name;
+        Birthdate = birthdate;
+        Email = email;
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Birthdate { get; set; }
+    public string Email { get; set; }
+}
